c91d26e9d130c00d78e58bb24c0efd9ff5de1f64
Jon McClennon <jon.mcclennon@gmail.com>
2019-06-17 17:18:32 +0200

    Updates

---
+++ precache-manifest.ee52cbb293d6de843a2ccf9b3cc6fce9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306b26886d9fc99e2451167d3e00fbdc",
+    "url": "/leaflet/index.html"
+  },
+  {
+    "revision": "21f26979355aec5347b9",
+    "url": "/leaflet/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "a512b9bba0b146a4265c",
+    "url": "/leaflet/static/css/main.09f7899a.chunk.css"
+  },
+  {
+    "revision": "21f26979355aec5347b9",
+    "url": "/leaflet/static/js/2.59175899.chunk.js"
+  },
+  {
+    "revision": "a512b9bba0b146a4265c",
+    "url": "/leaflet/static/js/main.b99f10c4.chunk.js"
+  },
+  {
+    "revision": "daf8075e4d168bc24d0b",
+    "url": "/leaflet/static/js/runtime~main.005ef40c.js"
+  },
+  {
+    "revision": "88b4fb36f220722b6b093ab03f0d1ab9",
+    "url": "/leaflet/static/media/empty-beer.88b4fb36.svg"
+  },
+  {
+    "revision": "a7a2fb8438c8aefac53b2d69b9458dd2",
+    "url": "/leaflet/static/media/filled-beer.a7a2fb84.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/leaflet/home/precache-manifest.1f3bd3173c8ee3aee5fa1f93b67e4279.js"
+  "/leaflet/precache-manifest.ee52cbb293d6de843a2ccf9b3cc6fce9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/leaflet/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/leaflet/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
