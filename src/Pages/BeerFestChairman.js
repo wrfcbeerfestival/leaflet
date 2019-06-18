b5c2b69b5f864abd77e06307a4039e6eb788bc37
Jon McClennon <jon.mcclennon@gmail.com>
2019-06-18 18:59:18 +0200

    Chairman information

--- src/Pages/BeerFestChairman.js
+++ src/Pages/BeerFestChairman.js
@@ -1,23 +1,19 @@
 import React from 'react';
+import { getImage } from '../image'
 
 export default () => {
-  return <section>
-    <h1>Beer festival chairman - Jon McClennon</h1>
+  return <section className="article">
+    <h1 className="article__title">Beer festival chairman</h1>
     <div>
-      <img className="image--full" src="/jon-mcclennon.jpg" />
+      <img className="image--full" src={ getImage('/jon-mcclennon.jpg')} />
     </div>
-    <p>Welcome to the end of the leaflet! Hopefully you’ve had a good flick through and have been impressed by the range of Beers and Ciders. Every year we say it’s the best selection, but somehow it gets better. This being our 6th event, we continue to grow not just in our selections of Beer and Cider, but in other aspects of the festival. Our 7’s tournament looks to be our biggest yet.</p>
-    <p>Last year I wrote a small introduction calling myself a Salmon. As you can guess a year on and I haven’t lived that down. Instead let's introduce some new faces of the WRFC family. Mr Ryan Dalton and his team. You may have noticed our clubhouse looks a bit different and that is due to their fine work. Here is a few words from Ryan;
-    </p>
-    <p>“Hello Beer & Cider lovers! My team and I have been working hard since last September to turn Watford Rugby Club into a unique social venue for the community that stretches further than just rugby. The mission isn’t over yet but we are getting there day by day, whilst most importantly, having fun along the way! After attending the beer festival last year, I have witnessed first hand the fantastic work Jon and the club done. Hopefully together we can make this years event the best yet!”
-</p>
-    <p>Now onto the event. As I said last year, I love it! I am a massive cider drinker and our range goes from strength to strength. Of course #beestingfriday will always have a place in my heart, but if you have the pleasure of trying Mango last year, you know the competition for festival favourite is stiff! However this year, I think we might have a few more dark horses; Cherry & Pineapple, Orange & Cinnamon and Pomegranate & Rose. I hope you will join me in trying them all.
-</p>
-    <p>Our event is only as good as it is thanks to the people who help out. The volunteers working the event keeping everyone nice and drunk, all the MMJ parents who cover the saturday shift so the seniors can run around in our pretty outfits pretending to play rugby. The guys who helped promote the event. The refs who step up every year, to ensure we have the highest standard of rugby, and are no way influenced by the number of jagers they are bought. Thank you to Sue for helping with the printing and Amber for all the design work. A huge thank you to our sponsors, the original Salmon Mr Geoff Hicks, and to all the people behind the scenes. Finally to Clint, a man who takes its on his shoulders, trying every single beer (multiple times) to ensure they are of the highest quality!
-</p>
-    <p>And finally to you. Without you here we wouldn’t have an event. You are the most important part of the whole festival. We hope we can count on your support throughout the season.
-</p>
-    Some stuff that ill say
+    <div className="article__content">
+      <p>Welcome to the digital leaflet! Something new that we are trying out this year. We are always trying to make our Beer & Cider festival even better and we hope this leaflet will help</p>
+      <p>You'll find everything good about the paper leaflet through the pages, plus some extra features. As you navigate through by swiping you'll find the beer and cider lists. Split by brewery you'll be able to rate each beer and add notes digital. All this information gets saved on your device and you can clear if you wish</p>
+      <p>This is my 3rd year running the event as it always makes me smile, just the amount of enthusiam that people have for the event. From the great range of beer and cider to seeing Mr Skinny Portway run around the pitch in some revealing outfit.</p>
+      <p>If this if your first time at the event, or you are a vet we would always love to hear from you. If thats to talk about the beer, help with selection or just a chat, WRFC is a club that accepts all.</p>
+    </div>
+    <div className="article__signature">Jon McClennon</div>
   </section>;
 }
 
